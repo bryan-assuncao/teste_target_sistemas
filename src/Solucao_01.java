2fa9a630cb869cfee6b9c4031c18c3fce5ba2c90
Bryan Assunção <bryan.assuncao@outlook.com.br>
2025-10-23 17:34:53 -0300

    first commit

---
+++ src/Solucao_01.java
@@ -0,0 +1,19 @@
+public class Solucao_01 {
+    public static void main(String[] args) {
+        System.out.println("""
+                \n1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+                Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+                Imprimir(SOMA);
+                Ao final do processamento, qual será o valor da variável SOMA?""");
+
+        int INDICE = 13;
+        int SOMA = 0;
+        int K = 0;
+
+        while (K < INDICE){
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+        System.out.println("\nO valor de SOMA é: " + SOMA);
+    }
+}
